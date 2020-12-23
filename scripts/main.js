8808506bc5b2c74fa7eba686812a7ecf32093721
asuradev99 <ethanaditya@gmail.com>
2020-12-23 12:29:18 -0700

    added name

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,27 @@
-const myHeading = document.querySelector("h1");
-const img = document.querySelector("img");
-
-myHeading.textContent = 'Hello World';
+let myHeading = document.querySelector("h1");
+let img = document.querySelector("img");
+let button = document.querySelector("button");
 
 img.onclick = function () {
     if (img.getAttribute("src") === "images/tomato.jpg") {
         img.setAttribute("src", "images/eggman.jpg");
     } else {
         img.setAttribute("src", "images/tomato.jpg");
-
     }
+}
+
+button.onclick = function () {
+    setUserName();
+}
+
+function setUserName () {
+    let myName = prompt("Please enter your name");
+    localStorage.setItem('name', myName)
+    myHeading.textContent = 'Welcome ' + myName;
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    myHeading.textContent = 'Welcome ' + myName;
 }
